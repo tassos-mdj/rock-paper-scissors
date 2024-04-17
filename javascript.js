4f1ff2f856f9e422fcb2a044308d7069e5bb02c7
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-04-17 16:39:31 +0300

    Create project, add first required fx

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];
+}
+
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
