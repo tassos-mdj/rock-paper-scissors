fafa6322ee6006f726473c2e6bd0ea5c1ea8cbd7
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-04-18 11:59:04 +0300

    Complete the Rock, Paper, Scissors project

--- javascript.js
+++ javascript.js
@@ -4,48 +4,67 @@ function getComputerChoice() {
     return choices[random];
 }
 
-const computerSelection = getComputerChoice();
-let playerInput = prompt("Please enter you selection!", "rock, paper or scissors?");
-console.log(computerSelection);
-const playerSelection = playerInput.toLowerCase();
+let computerScore = 0;
+let playerScore = 0;
 
 function playRound(playerSelection, computerSelection){
     let result;
     if (playerSelection === computerSelection){
-        result = "It/'s a draw!"; }
+        result = "It/'s a draw!";
+    }
     else { 
         if (computerSelection === "rock") {
             if (playerSelection === "paper"){
-                result = "You win! Paper beats rock."; 
+                result = "You win! Paper beats rock.";
+                playerScore += 1; 
             }
             else {
                 result = "You lose!Rock beats scissors";
+                computerScore += 1;
             }
         }
         else {
             if (computerSelection === "scissors") {
                 if (playerSelection === "rock"){
                     result = "You win! Rock beats scissors."; 
+                    playerScore += 1;
                 }
                 else {
                     result = "You lose! Scissors beat paper.";
+                    computerScore += 1;
                 }
             }
             else {
                 if (computerSelection === "paper"){
                     if (playerSelection === "scissors"){
-                        result = "You win! Scissors beats paper."; 
+                        result = "You win! Scissors beats paper.";
+                        playerScore += 1; 
                     }
                     else {
                         result = "You lose! Paper beats rock.";
+                        computerScore += 1;
                     } 
                     }
                 }
             }
         }
-    console.log(result);
+    
     return result;
     
     }
 
-    playRound(playerSelection, computerSelection);
+function playGame(){
+    let initGame = confirm("Do you want to start a new game?");
+    if (initGame) {
+        const computerSelection = getComputerChoice();
+        let playerInput = prompt("Please enter you selection!", "rock, paper or scissors?");
+        console.log(`Computer chose ${computerSelection}`);
+        const playerSelection = playerInput.toLowerCase();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`The score is player: ${playerScore} , computer: ${computerScore}`);
+        playGame();
+        }
+    else {console.log(`The game has ended. The final score is player: ${playerScore} , computer: ${computerScore}`)}
+    }
+
+playGame();
