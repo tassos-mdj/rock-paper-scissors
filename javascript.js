8311e4422a72a6f5320fbe1d2c61a8396bc499e8
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-05-06 16:37:56 +0300

    add ui functionality

--- javascript.js
+++ javascript.js
@@ -1,11 +1,26 @@
+let computerScore = 0;
+let playerScore = 0;
+let initGame = true;
+const resultsDisplay = document.querySelector("div");
+const newP = document.createElement("p");
+const newP2 = document.createElement("p");
+
+const buttons = document.querySelectorAll("button");
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.value);
+        playGame(button.value);
+        })
+});
+
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * choices.length);
     return choices[random];
 }
-
-let computerScore = 0;
-let playerScore = 0;
 
 function playRound(playerSelection, computerSelection){
     let result;
@@ -53,18 +68,31 @@ function playRound(playerSelection, computerSelection){
     
     }
 
-function playGame(){
-    let initGame = confirm("Do you want to start a new game?");
+    
+
+function playGame(playerChoice){
+    
+    
+    if (computerScore == 5 || playerScore == 5) {initGame = false};
+
     if (initGame) {
         const computerSelection = getComputerChoice();
-        let playerInput = prompt("Please enter you selection!", "rock, paper or scissors?");
-        console.log(`Computer chose ${computerSelection}`);
-        const playerSelection = playerInput.toLowerCase();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`The score is player: ${playerScore} , computer: ${computerScore}`);
-        playGame();
-        }
-    else {console.log(`The game has ended. The final score is player: ${playerScore} , computer: ${computerScore}`)}
+        let playerSelection = playerChoice;
+        resultsDisplay.textContent = `Computer chose ${computerSelection}`;
+        resultsDisplay.textContent += `, player chose ${playerSelection}`;
+        newP.textContent = `${playRound(playerSelection, computerSelection)}`;
+        newP2.textContent = `The score is player: ${playerScore} , computer: ${computerScore}`;
+        resultsDisplay.appendChild(newP);
+        resultsDisplay.appendChild(newP2);
+                    
+    }
+    else {
+        newP2.textContent = `The game has ended. The final score is player: ${playerScore} , computer: ${computerScore}`;
+        resultsDisplay.appendChild(newP2);
+        
+    }
     }
 
-playGame();
+
+
+
